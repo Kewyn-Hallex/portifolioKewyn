50cef2b75d7d789c0dbecc34e2b84ab2dbc63c45
Kewyn <hallexkewyn@gmail.com>
2025-04-12 18:11:02 -0300

    commit de atualizacao de interface e animacoes

--- script/scroll.js
+++ script/scroll.js
@@ -1,13 +1,16 @@
 const myObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting){
-            entry.target.classList.add('show')
+            entry.target.classList.add('show');
+            if (entry.target.classList.contains('hidden-right')) {
+                entry.target.classList.add('show-right');
+            }
         } else {
-            entry.target.classList.remove('show')
+            entry.target.classList.remove('show', 'show-right');
         }
-    })
-})
+    });
+});
 
-const elements = document.querySelectorAll('.hidden')
+const elements = document.querySelectorAll('.hidden, .hidden-right');
 
-elements.forEach((element) => myObserver.observe(element))
+elements.forEach((element) => myObserver.observe(element));
